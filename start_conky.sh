d5aab4dfb466024678a7f0d5ff4526369d2e5a48
wo2ni <i_o_i_o@163.com>
2021-01-22 18:10:18 +0800

    start conky

--- start_conky.sh
+++ start_conky.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/usr/bin/conky -c ~/.conky/system-monitoring/run_wlan0.conkyrc &
-/usr/bin/conky -c ~/.conky/system-monitoring/cpu.conkyrc &
-/usr/bin/conky -c ~/.conky/parrot-monitoring/run.conkyrc &
+/usr/bin/conky -c ~/.conky/system-monitoring/run_wlan0.conkyrc > /dev/null 2>&1 &
+/usr/bin/conky -c ~/.conky/system-monitoring/cpu.conkyrc > /dev/null 2>&1 &
+/usr/bin/conky -c ~/.conky/parrot-monitoring/run.conkyrc > /dev/null 2>&1 &
