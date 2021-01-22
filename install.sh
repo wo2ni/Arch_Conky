f0a3a78da87129c215dcf4e0a014fd9508cd5584
wo2ni <i_o_i_o@163.com>
2021-01-22 18:10:47 +0800

    install

--- install.sh
+++ install.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+Config_conky() {
+    mv -v parrot-monitoring/ ~/.conky/
+    mv -v system-monitoring/ ~/.conky/
+    chmod +x start_conky.sh 
+    mv -v start_conky.sh ~/.conky/
+    cat << EOF >> ~/.bashrc
+    alias conky='~/.conky/start_conky.sh '
+EOF
+}
+
+if [ -d ~/.conky ]; then
+    Config_conky
+else
+    mkdir ~/.conky
+    Config_conky
+fi
